c9828050ca8765c442b6502face943dafe8afd36
Lala <laura.hreniucu@gmail.com>
2017-09-02 15:13:06 +0200

    Use blog page as index

--- routes/index.js
+++ routes/index.js
@@ -34,10 +34,9 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = function (app) {
 	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
 	app.all('/contact', routes.views.contact);
+	app.get('/post/:post', routes.views.post);
+	app.get('/:category?', routes.views.blog);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
